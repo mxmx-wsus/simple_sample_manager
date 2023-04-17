cc04e31866aedf1d00946c2bdb5c60ccff6e91cf
ryota masuyama <mxmx.khaki@gmail.com>
2023-04-17 21:42:26 +0900

    Bank選択画面のボタンの動作修正

--- lib/component/bank_button.dart
+++ lib/component/bank_button.dart
@@ -24,10 +24,6 @@ class BankButton extends StatelessWidget {
       ),
       onPressed: () {
         //ボタンが押されたら「textfield.dart」のDisplauControllerに「_keyWord」を追加する。
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const SelectPadPage()),
-        );
       },
       child: Text(
         _keyWord,
